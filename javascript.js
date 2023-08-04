fff2197e6ec049a8c0b43f099cfef6ac4146ea8f
daltonoswald <daltonoswald@gmail.com>
2023-08-04 12:55:32 -0400

    nearly finished everything

---
+++ javascript.js
@@ -0,0 +1,146 @@
+let library = document.getElementById('library');
+library.innerHTML = "";
+const newBookForm = document.getElementById('newBookForm')
+
+newForm.onsubmit= addBook;
+
+function Book(id, title, author, pages, read) {
+    this.id = id;
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+
+
+let myLibrary = [];
+
+let idCount = myLibrary.length +1;
+const theHobbit = new Book(idCount, 'The Hobbit', 'J. R. R. Tolkien', 295, false);
+myLibrary.push(theHobbit);
+createBookCard(idCount, 'The Hobbit', 'J. R. R. Tolkien', 295, false)
+idCount = myLibrary.length +1;
+const toKillAMockingbird = new Book(idCount, 'To Kill a Mockingbird', 'Harper Lee', 336, true);
+myLibrary.push(toKillAMockingbird);
+createBookCard(idCount, 'To Kill a Mockingbird', 'Harper Lee', 336, true);
+
+function bookForm() {
+    let newForm = document.getElementById('newForm');
+    if (newForm.style.display === "none"){
+        newForm.style.display = "flex"
+    } else {
+        newForm.style.display = "none";
+    }
+}
+
+
+// const getBook = () => {
+//     const id = myLibrary.length +1;
+//     const title = document.getElementById('title').value
+//     const author = document.getElementById('author').value
+//     const pages = document.getElementById('pages').value
+//     const read = document.getElementById('read').value
+//     return new Book(id, title, author, pages, read);
+// }
+
+function addBook (e) {
+    e.preventDefault();
+    idCount++;
+    let titleText = document.querySelector('#title');
+    let authorText = document.querySelector('#author');
+    let pagesText = document.querySelector('#pages');
+    let readText = document.querySelector('#read');
+    const newBook = new Book((idCount), titleText.value, authorText.value, pagesText.value, readText.checked);
+    myLibrary.push(newBook);
+    createBookCard((idCount), titleText.value, authorText.value, pagesText.value, readText.checked)
+    console.log(myLibrary);
+
+    clearForm();
+    let newForm = document.getElementById('newForm');
+    newForm.style.display = "none";
+}
+
+function clearForm() {
+    document.getElementById("newForm").reset();
+}
+
+function removeBook(e) {
+    let result = confirm("Are you sure you want to delete this book?");
+    if (result == true) {
+    targetId = parseInt(e.target.parentElement.id,10);
+    for (i = myLibrary.length -1; i >= 0; --i){
+        if (myLibrary[i].id === targetId) {
+            myLibrary.splice(i,1)
+        }
+    }
+    e.target.parentElement.remove();
+    } else {
+        return
+    }
+}
+
+function readToggle(e) {
+    targetId = parseInt(e.target.parentElement.id,10);
+    for (i = myLibrary.length -1; i >= 0; --i){
+        if (myLibrary[i].id === targetId) {
+            myLibrary[i].read = !myLibrary[i].read;
+        }
+    }
+}
+
+function createBookCard(id, title, author, pages, read) {
+    const bookCard = document.createElement('div');
+    bookCard.setAttribute('id', id);
+    bookCard.classList.add('bookCard')
+
+    let titleElement = document.createElement('p');
+    titleElement.classList.add('title');
+    let authorElement = document.createElement('p');
+    authorElement.classList.add('author');
+    let pagesElement = document.createElement('p');
+    pagesElement.classList.add('pages');
+    // let readElement = document.createElement('p');
+    let readToggleElement = document.createElement('button');
+    let removeBtnElement = document.createElement('button');
+    removeBtnElement.classList.add('remove')
+
+    titleElement.textContent = `${title}`;
+    authorElement.textContent = `${author}`;
+    pagesElement.textContent = `${pages} Pages`;
+
+    if (read) {
+        readToggleElement.textContent = `Read`;
+        readToggleElement.classList.add('haveRead')
+    } else {
+        readToggleElement.textContent = 'Not Read'
+        readToggleElement.classList.add('notRead')
+    }
+    readToggleElement.addEventListener('click', function(event){
+        readToggle(event);
+    });
+    readToggleElement.addEventListener('click', () => {
+        if (readToggleElement.classList.contains('haveRead')){
+            readToggleElement.classList.add('notRead');
+            readToggleElement.classList.remove('haveRead');
+            readToggleElement.textContent = `Not Read`;
+        } else {
+            readToggleElement.classList.remove('notRead');
+            readToggleElement.classList.add('haveRead');
+            readToggleElement.textContent = 'Read';
+        }
+    });
+
+    // readElement.innerHTML = `${read}`;
+    removeBtnElement.textContent = "Remove";
+
+    library.appendChild(bookCard);
+    bookCard.appendChild(titleElement);
+    bookCard.appendChild(authorElement);
+    bookCard.appendChild(pagesElement);
+    bookCard.appendChild(readToggleElement);
+    bookCard.appendChild(removeBtnElement);
+
+    removeBtnElement.addEventListener('click', removeBook);
+}
+
+
